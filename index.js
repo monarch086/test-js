7743f3da65df5830d4f48400fcf31999575f9b58
Oleksandr Barsuk <Oleksandr_Barsuk@epam.com>
2021-05-13 11:40:33 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// async function f() {
+
+//   let promise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("готово!"), 1000)
+//   });
+
+//   let result = await promise; // будет ждать, пока промис не выполнится (*)
+
+//   alert(result); // "готово!"
+// }
+
+// f();
+
+async function showAvatar() {
+
+    // запрашиваем JSON с данными пользователя
+    //let response = await fetch('user.json');
+    //let user = await response.json();
+    let user = {
+        "name": "iliakan",
+        "isAdmin": true
+    };
+  
+    // запрашиваем информацию об этом пользователе из github
+    let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+    let githubUser = await githubResponse.json();
+  
+    // отображаем аватар пользователя
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = "promise-avatar-example";
+    document.body.append(img);
+  
+    // ждём 3 секунды и затем скрываем аватар
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+  
+    img.remove();
+  
+    return githubUser;
+  }
+  
+  showAvatar();
